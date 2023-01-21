d0ff52ebfc86a69003361607cccb2e8bf63f69d3
Alan <alanlichen1@gmail.com>
2023-01-21 10:25:18 -0800

    :sparkles: Add new difficulties to game

--- gameplugin/game.go
+++ gameplugin/game.go
@@ -9,6 +9,7 @@ import (
 	"github.com/disgoorg/handler"
 	"github.com/disgoorg/json"
 	"github.com/fogleman/gg"
+	"image"
 	"io"
 	"math/rand"
 	"strconv"
@@ -20,26 +21,28 @@ type AnswerChoice struct {
 	IsCorrect bool
 }
 
+type Difficulty int
+
+const (
+	DifficultyNormal Difficulty = iota
+	DifficultyPixelShuffle
+	DifficultyDiscoloration
+	DifficultyCombo
+)
+
 func GetAnswerSet() ([]AnswerChoice, int) {
 	var answerSet []AnswerChoice
 	answersChose := make(map[string]int)
 	bees := loaders.GetBeeNames()
-	correctDone := false
-	correctI := 0
+	correctI := rand.Intn(4)
 	for i := 0; i < 4; {
 		randomIndex := rand.Intn(len(bees))
 		if _, found := answersChose[bees[randomIndex]]; found {
 			continue
 		}
-		correct := false
-		if !correctDone && rand.Intn(2) == 1 {
-			correct = true
-			correctDone = true
-			correctI = i
-		}
 		answerSet = append(answerSet, AnswerChoice{
 			Name:      bees[randomIndex],
-			IsCorrect: correct,
+			IsCorrect: i == correctI,
 		})
 		answersChose[bees[randomIndex]] = 1
 		i++
@@ -47,10 +50,49 @@ func GetAnswerSet() ([]AnswerChoice, int) {
 	return answerSet, correctI
 }
 
-func GetAnswerSetButtons(userId string) ([]discord.InteractiveComponent, io.Reader) {
+func DiscolorImage(img image.Image) image.Image {
+	dc := gg.NewContextForImage(img)
+	dc.SetLineWidth(3)
+	for i := 0; i < 80; i++ {
+		dc.SetRGB(rand.Float64(), rand.Float64(), rand.Float64())
+		dc.DrawLine(rand.Float64()*float64(img.Bounds().Max.X), rand.Float64()*float64(img.Bounds().Max.Y), rand.Float64()*float64(img.Bounds().Max.X), rand.Float64()*float64(img.Bounds().Max.Y))
+		dc.Stroke()
+	}
+	return dc.Image()
+}
+
+func ShuffleImage(img image.Image) image.Image {
+	dc := gg.NewContext(img.Bounds().Max.X, img.Bounds().Max.Y)
+	dc.DrawImage(img, 0, 0)
+	for i := 0; i < 200; i++ {
+		x1 := rand.Intn(img.Bounds().Max.X)
+		y1 := rand.Intn(img.Bounds().Max.Y)
+		x2 := rand.Intn(img.Bounds().Max.X)
+		y2 := rand.Intn(img.Bounds().Max.Y)
+		c1 := img.At(x1, y1)
+		c2 := img.At(x2, y2)
+		dc.SetColor(c1)
+		dc.DrawRectangle(float64(x2), float64(y2), float64(rand.Intn(10)), float64(rand.Intn(10)))
+		dc.SetColor(c2)
+		dc.DrawRectangle(float64(x1), float64(y1), float64(rand.Intn(10)), float64(rand.Intn(10)))
+	}
+	dc.Fill()
+	return dc.Image()
+}
+
+func GetAnswerSetButtons(userId string, difficulty Difficulty) ([]discord.InteractiveComponent, io.Reader) {
 	answerSet, correctI := GetAnswerSet()
 	//fmt.Println("correctI: "+strconv.Itoa(correctI), answerSet[correctI].Name)
 	img, err := gg.LoadImage("assets/bees/" + answerSet[correctI].Name + ".png")
+	switch difficulty {
+	case DifficultyPixelShuffle:
+		img = ShuffleImage(img)
+	case DifficultyDiscoloration:
+		img = DiscolorImage(img)
+	case DifficultyCombo:
+		img = ShuffleImage(img)
+		img = DiscolorImage(img)
+	}
 	if err != nil {
 		fmt.Println(err, answerSet[correctI].Name)
 		img, _ = gg.LoadImage("assets/error.png")
@@ -67,9 +109,9 @@ func GetAnswerSetButtons(userId string) ([]discord.InteractiveComponent, io.Read
 	for i, answer := range answerSet {
 		id := ""
 		if answer.IsCorrect {
-			id = "handler:correct:" + userId
+			id = "handler:correct:" + userId + ":" + strconv.Itoa(int(difficulty))
 		} else {
-			id = "handler:incorrect:" + userId + ":" + string(i)
+			id = "handler:incorrect:" + userId + ":" + strconv.Itoa(int(difficulty)) + ":" + strconv.Itoa(i)
 		}
 		buttons = append(buttons, discord.ButtonComponent{
 			Label:    answer.Name,
@@ -97,6 +139,29 @@ func GameCommand(b *common.Bot, gameService *State) handler.Command {
 							MinValue:    json.Ptr(5),
 							MaxValue:    json.Ptr(25),
 						},
+						discord.ApplicationCommandOptionInt{
+							Name:        "difficulty",
+							Description: "Difficulty of the game",
+							Required:    false,
+							Choices: []discord.ApplicationCommandOptionChoiceInt{
+								{
+									Name:  "Normal",
+									Value: 0,
+								},
+								{
+									Name:  "Difficult",
+									Value: 1,
+								},
+								{
+									Name:  "Insane",
+									Value: 2,
+								},
+								{
+									Name:  "Expert",
+									Value: 3,
+								},
+							},
+						},
 					},
 				},
 			},
@@ -108,8 +173,12 @@ func GameCommand(b *common.Bot, gameService *State) handler.Command {
 				if !ok {
 					questions = 5
 				}
+				difficulty, ok := data.OptInt("difficulty")
+				if !ok {
+					difficulty = 0
+				}
 				gameService.StartTriviaGame(event.User().ID, questions)
-				buttons, r := GetAnswerSetButtons(event.User().ID.String())
+				buttons, r := GetAnswerSetButtons(event.User().ID.String(), Difficulty(difficulty))
 				return event.CreateMessage(discord.MessageCreate{
 					Embeds: []discord.Embed{
 						{
@@ -135,7 +204,8 @@ func GameCommand(b *common.Bot, gameService *State) handler.Command {
 }
 
 func TriviaButtonHandler(event *events.ComponentInteractionCreate) error {
-	buttons, r := GetAnswerSetButtons(event.User().ID.String())
+	difficulty, _ := strconv.Atoi(strings.Split(event.ButtonInteractionData().CustomID(), ":")[3])
+	buttons, r := GetAnswerSetButtons(event.User().ID.String(), Difficulty(difficulty))
 	return event.UpdateMessage(discord.MessageUpdate{
 		Embeds: &[]discord.Embed{
 			{
