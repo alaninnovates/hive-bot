d5ee9254ef3482f0471455d4a2007aa667113e85
Alan <alanlichen1@gmail.com>
2024-06-02 18:27:26 -0700

    :sparkles: Premium users get up to 25 saves

--- common/config.go
+++ common/config.go
@@ -3,8 +3,9 @@ package common
 import "github.com/disgoorg/disgo/discord"
 
 var (
-	MaxFreeSaves   = 5
-	LinksActionRow = discord.ActionRowComponent{
+	MaxFreeSaves    = 5
+	MaxPremiumSaves = 25
+	LinksActionRow  = discord.ActionRowComponent{
 		discord.NewLinkButton("Documentation", "https://hive-builder.alaninnovates.com/"),
 		discord.NewLinkButton("Support server", "https://discord.gg/meta-bee-995988457136603147"),
 	}
